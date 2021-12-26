25abbd3e2f714f00a11172edd5b6b475e5a8e091
Ilan Grynszpan <ilan.grynszpan@gmail.com>
2021-12-25 23:35:25 -0300

    master updated, visuals matching initial website

--- src/pages/home/HomeCreditOnly/index.js
+++ src/pages/home/HomeCreditOnly/index.js
@@ -4,7 +4,7 @@ import { Grid, Container, Row, Col,Button,Badge } from 'react-bootstrap';
 import TitleBar from '../../../components/TitleBar';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import Resources from '../../../Resources';
+import Resources, { urls } from '../../../Resources';
 
 import './HomeCreditOnly.css';
 
@@ -69,12 +69,16 @@ export default () => {
 
             <section className="cta" style={{backgroundColor:"#7DC7D0"}}>
                 <h5>Clique no botão se você tem uma empresa com presença digital</h5>
-                <Button variant="primary">Pedir crédito</Button>
+                <a href={urls.form_companhias}>
+                    <Button variant="primary">Pedir crédito</Button>
+                </a>
             </section>
 
             <section className="cta" style={{backgroundColor:"#C5DADC"}}>
                 <h5>Clique no botão se você é uma pessoa gestora de tráfego</h5>
-                <Button variant="primary">Pedir crédito</Button>
+                <a href={urls.form_gestores_trafego}>
+                    <Button variant="primary">Pedir crédito</Button>
+                </a>
             </section>
 
             <section className='parceiros'>
